dd7a223494987583ade624e49e14bb29978cc7e1
Erisi <124305740+riski10alt@users.noreply.github.com>
2023-05-21 18:48:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// let opened = false;
+// function myFunction(x) {
+//   x.classList.toggle("change");
+//   opened = !opened;
+//   opened ? document.getElementsByTagName('nav')[0].style.width = "20%" : document.getElementsByTagName('nav')[0].style.width = "64px" ; 
+//   let elems = document.getElementsByClassName('nav-link');
+//   for ( let e of elems ) {
+//     opened ? e.style.display = "block" : e.style.display = "none";
+//   }
+// }
